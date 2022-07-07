b73e16dd3e49b18ee0c453f80faecff72b222803
timcs <timcs1274@gmail.com>
2022-07-06 21:22:48 -0400

    add uv index classes and loop which applies uv color class

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,18 @@
 var nycCall = "https://api.openweathermap.org/data/2.5/onecall?lat=40.47&lon=73.58&exclude={part}&appid=d9ddba3de900f79669d3b449fadefd3e"
 console.log(nycCall)
 
-	
+
+
+
+//uv index color change
+var uvValue = ""
+for (i = 0; i <=2;){    
+    if (uvValue) {
+        $("#uv-index-container").addClass("uv-low");
+    } else if (uvValue <=5) {
+        $("#uv-index-container").addClass("uv-moderate");
+    }
+    else if (uvValue>5) {
+        $("#uv-index-container").addClass("uv-high");
+    }
+}
