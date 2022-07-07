f45f0f2c07c33fb4001d9c4ceeea2e6060d849af
timcs <timcs1274@gmail.com>
2022-07-07 00:13:16 -0400

    added 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //https://api.openweathermap.org/data/2.5/onecall?lat={40.47}&lon={73.58}&exclude={part}&appid={API key}
-var nycCall = "https://api.openweathermap.org/data/2.5/onecall?lat=40.47&lon=73.58&exclude={part}&appid=d9ddba3de900f79669d3b449fadefd3e"
+var nycCall = "https://api.openweathermap.org/data/2.5/onecall?lat=40.47&lon=73.58&exclude=minutely,hourly&appid=d9ddba3de900f79669d3b449fadefd3e"
 console.log(nycCall)
 
 
