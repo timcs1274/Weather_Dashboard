40bdabdc95af6dc75a15ca52716ac15f8cafa05f
timcs <timcs1274@gmail.com>
2022-08-30 11:01:30 -0400

    function for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,5 +36,30 @@ function currentWeather(city){
       var ws=response.wind.speed;
       var windsmph=(ws*2.237).toFixed(1);
       $(currentWSpeed).html(windsmph+"MPH");
+      
   });
 }
+
+function forecast(cityid){
+  var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+  $.ajax({
+      url:queryforcastURL,
+      method:"GET"
+  }).then(function(response){
+      
+      for (i=0;i<5;i++){
+          var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+          var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+          var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+          var tempK= response.list[((i+1)*8)-1].main.temp;
+          var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+          var humidity= response.list[((i+1)*8)-1].main.humidity;
+      
+          $("#fDate"+i).html(date);
+          $("#fImg"+i).html("<img src="+iconurl+">");
+          $("#fTemp"+i).html(tempF+"&#8457");
+          $("#fHumidity"+i).html(humidity+"%");
+      }
+  });
+}
+
