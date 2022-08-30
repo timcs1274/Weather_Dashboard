4fcfbdf82df25e8618c260f316ec91fca2130363
timcs <timcs1274@gmail.com>
2022-08-30 10:37:55 -0400

    add variables and api key

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var city="";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+
+var APIKey="d9ddba3de900f79669d3b449fadefd3e";
+
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
